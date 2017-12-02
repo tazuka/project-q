790bb20f80e2ab1b12be0c4b1c0eef56d34e570b
Mervlim <melzlim@hotmail.com>
2017-12-02 22:05:24 +0800

    added admin dashboard

---
+++ src/components/admindashboard/admindashboard.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+export class admindashboard extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (<div>MyComponent</div>);
+  }
+}
+
+export default admindashboard;
